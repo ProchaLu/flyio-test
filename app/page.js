b704bca6ebc687fec3fbd8d90c78603deb92b7b0
Lukas Prochazka <prochazka.lu@gmail.com>
2025-01-08 16:43:12 +0100

    Update deps and add image

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Fly.io test repo</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={200} height={200} />
     </div>
   );
 }
